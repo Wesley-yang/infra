2c574079d7eedc2f167d5fb3452b5a224cdef911
Jing Lu <lvjing5@huawei.com>
2020-03-11 11:35:51 +0800

    Create install_docker-ce_ubuntu.sh

---
+++ tools/install_docker-ce_ubuntu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2019 Huawei Technologies Co., Ltd. and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -x
+
+# Update the apt package index
+apt-get update
+
+# Install packages to allow apt to use a repository over HTTPS
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker’s official GPG key  
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the stable repository
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Update the apt package index
+apt-get update
+
+# Install the latest version of Docker Engine - Community and containerd
+apt-get install -y docker-ce docker-ce-cli containerd.io
