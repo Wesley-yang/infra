bca19810d2a4dd76008abcbb3cf31c830901245a
Jing Lu <lvjing5@huawei.com>
2020-03-11 11:36:36 +0800

    Create  install_docker-ce_centos.sh

---
+++ tools/install_docker-ce_centos.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2019 Huawei Technologies Co., Ltd. and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -x
+
+# Uninstall old versions
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+                  
+# Install required packages
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+# Set up the stable repository.
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+  
+# Install the latest version of Docker Engine  
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+# Start Docker
+sudo systemctl start docker
+sudo systemctl enable docker
