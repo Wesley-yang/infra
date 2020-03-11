cfaa98518f2b8e678f256dffcc29cd8695081adb
Jing Lu <lvjing5@huawei.com>
2020-03-11 11:34:54 +0800

    Create install_docker_compose_linux.sh

---
+++ tools/install_docker_compose_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2019 Huawei Technologies Co., Ltd. and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -x
+
+# Download the current stable release of Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
